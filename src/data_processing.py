b5c345e10337951d91ef0b4df5363d95bf06dab6
Sejal21-creator <jainsejal042004@gmail.com>
2026-04-04 00:17:09 +0530

    Create data_processing.py

---
+++ src/data_processing.py
@@ -0,0 +1,16 @@
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r'<.*?>', '', text)
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+    text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub(r'\d+', '', text)
+    text = ' '.join(word for word in text.split() if word not in stop_words)
+    return text
